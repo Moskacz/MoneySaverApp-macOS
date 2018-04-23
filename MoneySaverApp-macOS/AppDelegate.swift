5558ae2bb9f7c9b66627762afd9fc5d268511ba7
Michał Moskała <moskala.michal@gmail.com>
2018-04-23 21:09:26 +0200

    macOS project created from template

---
+++ MoneySaverApp-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MoneySaverApp-macOS
+//
+//  Created by Michal Moskala on 23.04.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MoneySaverApp-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoneySaverApp-macOS
+//
+//  Created by Michal Moskala on 23.04.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
